fbb6eb316ac9abc6e402013d3342b32562fa1c98
Jalen-Foreman <jalenforeman594@gmail.com>
2021-03-11 13:36:40 -0500

    Nav bar + urlclick + form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import LocationForm from './components/LocationForm'
 import './App.css';
 
 function App() {
+  const [quality, setQuality] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Link to="/">Home|</Link>
+        <Link to="/PM">What is a Pariculate Matter (PM)?|</Link>
+        <Link to="/contact">Contact|</Link>
+        <Link to="/about">About</Link>
+      </nav>
+        <LocationForm />
+      <main></main>
     </div>
   );
 }
---
+++ src/components/LocationForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LocationForm = () => {
+    return (
+        <div>
+            <form>
+                <h2>Location</h2>
+                <input required placeholder="Hardcoded location data" />
+            </form>
+        </div>
+    );
+};
+
+export default LocationForm;
